6b2401e9b144f3459fe0774a31caaad99d035c70
amit <amit@finsynth.ai>
2025-08-15 23:09:49 +0530

    design folder structure and implement pdf-file text extraction

--- app/app.py
+++ app/app.py
@@ -1,3 +1,14 @@
 import streamlit as st
+from pdf_utils import extract_text_from_pdf
 
-st.write("Hello, Streamlit!")
+st.set_page_config(page_title="PDF Reader", layout="wide")
+st.title("PDF text Extracter")
+
+uploaded_file = st.file_uploader("Upload a PDF file", type="pdf")
+
+
+if uploaded_file is not None:
+    with st.spinner("Extracting text ....."):
+        extracted_text = extract_text_from_pdf(uploaded_file)
+        st.success("✅ Extraction complete!")
+        st.text_area("Extracted Text", extracted_text, height=400)
